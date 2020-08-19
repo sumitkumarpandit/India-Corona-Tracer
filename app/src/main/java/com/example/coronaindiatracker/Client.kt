fdcc7ebe417558a2b4ad8557a9bae6f9838a9629
panditroxx <sumitkumarpandit434@gmail.com>
2020-08-19 18:17:52 +0530

    Simple Covid19 Dashboard

---
+++ app/src/main/java/com/example/coronaindiatracker/Client.kt
@@ -0,0 +1,9 @@
+package com.example.coronaindiatracker
+import okhttp3.Request
+import okhttp3.OkHttpClient
+object Client {
+    private val okHttpClient = OkHttpClient()
+    private val request = Request.Builder()
+        .url("https://api.covid19india.org/data.json").build()
+    val api = okHttpClient.newCall(request)
+}
---
+++ app/src/main/java/com/example/coronaindiatracker/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.coronaindiatracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listAdapter:ListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO){Client.api.execute()}
+            if(response.isSuccessful){
+                val data = Gson().fromJson(response.body?.string(),Response::class.java)
+                launch(Dispatchers.Main){
+                    bindCombinedData((data.statewise[0]))
+                    bindStateWiseData(data.statewise.subList(0,data.statewise.size))
+                }
+            }
+        }
+    }
+    private fun bindStateWiseData(subList:List<StatewiseItem>){
+        listAdapter = ListAdapter(subList)
+        list.addHeaderView(LayoutInflater.from(this).inflate(R.layout.list_header,list,false))
+        list.adapter = listAdapter
+    }
+    private fun bindCombinedData(data:StatewiseItem){
+        confirmedTv.text = data.confirmed
+        activeTv.text = data.active
+        recoveredTv.text = data.recovered
+        deceasedTv.text= data.deaths
+    }
+}
